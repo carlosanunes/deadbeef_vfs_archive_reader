eee63ee3cde41d9f1de9b84e499756883ba92de0
carlos.nunes <carlos.nunes@e452b2a4-3fef-4c0f-9a7f-398e19c9e114>
2011-07-20 13:15:22 +0000

    First commit

---
+++ trunk/src/vfs_archive_reader.c
@@ -0,0 +1,262 @@
+#include <deadbeef/deadbeef.h>
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define trace(...) { fprintf(stderr, __VA_ARGS__); }
+//#define trace(fmt,...)
+
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#include "..//File_Extractor-1.0.0/fex/fex.h"
+
+static DB_functions_t *deadbeef;
+static DB_vfs_t plugin;
+
+typedef struct {
+    DB_FILE file;
+    fex_t* archive;
+    fex_pos_t pos;
+    int64_t size;
+} archive_file_t;
+
+
+static const char *scheme_names[] = { "rar://", "7z://", "gz://", NULL };
+
+const char **
+vfs_archive_reader_get_schemes (void) {
+    return scheme_names;
+}
+
+int
+vfs_archive_reader_is_streaming (void) {
+    return 0;
+}
+
+// fname must have form of rar://full_filepath.rar:full_filepath_in_rar
+DB_FILE*
+vfs_archive_reader_open (const char *fname) {
+
+	if ( (strncasecmp(fname, "rar://", 6) != 0 ) && (strncasecmp(fname, "7z://", 5) != 0 ) && (strncasecmp(fname, "gz://", 5) != 0  ))
+		{ return NULL; }    
+
+    if (strncasecmp (fname, "rar://", 6) == 0)
+	fname += 6;
+    else
+	fname += 5;
+
+    const char *colon = strchr (fname, ':');
+    if (!colon) {
+        return NULL;
+    }
+
+    char archname[colon-fname+1];
+    memcpy (archname, fname, colon-fname);
+    archname[colon-fname] = 0;
+
+    fname = colon+1;
+
+ 	fex_t* fex;
+	if (fex_open( &fex, archname) != NULL ) /* opening the rar */
+		{ fex_close(fex); return NULL; }
+
+	/* the library doesn have a built in search function so */
+	int n = 0; 
+	while ( !fex_done( fex ) )
+	{
+		if(strcmp(fex_name(fex), fname) == 0 )
+			break; /* found the one */
+
+		++n;
+		fex_next( fex );
+	}
+	// sanity check: are we at the end of the archive?
+	if(fex_done(fex))
+		{ fex_close(fex); return NULL; }
+
+	if (fex_stat(fex) != NULL)
+		{ fex_close(fex); return NULL; }
+
+    archive_file_t *f = malloc (sizeof (archive_file_t));
+    memset (f, 0, sizeof (archive_file_t));
+    f->size = (int64_t) fex_size(fex);
+    f->archive = fex;
+    f->pos = fex_tell_arc(fex);
+    f->file.vfs = &plugin;	
+
+    return (DB_FILE*)f;
+
+}
+
+void
+vfs_archive_reader_close (DB_FILE *f) {
+	archive_file_t *arc = (archive_file_t *)f;
+	fex_close(arc->archive);
+	free(arc);
+}
+
+size_t
+vfs_archive_reader_read (void *ptr, size_t size, size_t nmemb, DB_FILE *f) {
+	archive_file_t *arc = (archive_file_t *)f;
+
+	size_t rb = (size_t)fex_tell(arc->archive);
+	int n = min ( size * nmemb, arc->size - rb );	
+	assert ( fex_read( arc->archive, ptr, n ) == NULL);
+	rb = (size_t) fex_tell(arc->archive) - rb;
+
+// printf("read: %s readbytes: %i sz: %i psize:%i nmemb:%i fex_tell:%i\n", ptr, rb, n, size, nmemb, fex_tell(arc->archive));
+	return rb/size;
+}
+
+int
+vfs_archive_reader_seek (DB_FILE *f, int64_t offset, int whence) {
+	archive_file_t *arc = (archive_file_t *)f;
+/*
+switch( whence ) {
+	case SEEK_CUR:
+		printf("cur\n"); break;
+	case SEEK_END:
+		printf("end\n"); break;
+	case SEEK_SET:
+		printf("start\n"); break;
+
+}
+printf("seek offset: %i\n", offset);
+*/
+	int64_t tell = (int64_t) fex_tell(arc->archive); /* for better readibility */
+
+    if (whence == SEEK_CUR) {
+        offset += tell;
+    } else if (whence == SEEK_END) {
+        offset += (int64_t) arc->size;
+    }
+
+    /* have to go back in file */
+	if (offset < tell) {
+		// reopen
+		if (fex_seek_arc( arc->archive, arc->pos ) != NULL)
+			return -1;
+    }
+
+	char buf[4096];
+	// offset is the desired tell
+	int64_t n = offset - (int64_t) fex_tell(arc->archive);
+	while( n > 0 ) {
+		int64_t sz = min(n, sizeof(buf));
+	/* fex_read, tries to read n bytes even if there are not that many to read */
+		sz = min (sz, arc->size - fex_tell(arc->archive) );
+		assert(fex_read(arc->archive, buf, sz) == NULL);
+		// end of file
+		if ( sz < min (n, sizeof(buf)) )
+			break;
+
+		n = offset - (int64_t) fex_tell(arc->archive);
+	}
+
+//	printf("foffset %i\n", (int64_t) fex_tell(arc->archive));
+
+    if (n > 0) {
+        return -1;
+    }
+    return 0;
+
+}
+
+int64_t
+vfs_archive_reader_tell (DB_FILE *f) {
+    archive_file_t *arc = (archive_file_t *)f;
+    return (int64_t) fex_tell( arc->archive );
+}
+
+
+void
+vfs_archive_reader_rewind (DB_FILE *f) {
+	archive_file_t *arc = (archive_file_t *)f;
+	assert (fex_seek_arc( arc->archive, arc->pos ) == NULL);
+}
+
+
+int64_t
+vfs_archive_reader_getlength (DB_FILE *f) {
+	archive_file_t *arc = (archive_file_t *)f;
+    return arc->size;
+}
+
+/* getting the contents  */
+int
+vfs_archive_reader_scandir (const char *dir, struct dirent ***namelist, int (*selector) (const struct dirent *), int (*cmp) (const struct dirent **, const struct dirent **)) {
+	
+	fex_t* fex;
+	if (fex_open( &fex, dir) != NULL )
+		return -1;
+	
+	int n = 0;
+	char * ext = NULL;
+	while ( !fex_done(fex) )
+	{
+		*namelist = realloc (*namelist, sizeof (void *) * (n+1) );
+		(*namelist)[n] = malloc (sizeof (struct dirent));
+		memset ((*namelist)[n], 0, sizeof (struct dirent));
+		if (fex_has_extension( dir, "rar" ))
+			ext = "rar";
+		else if (fex_has_extension(dir, "gz"))
+			ext = "gz";
+		else
+			ext = "7z";
+		snprintf ((*namelist)[n]->d_name, sizeof ((*namelist)[n]->d_name), "%s://%s:%s", ext, dir, fex_name(fex) );
+
+		fex_next(fex);
+		++n;
+	}
+
+	fex_close(fex);
+
+	return n;
+}
+
+
+int
+vfs_archive_reader_is_container (const char *fname) {
+    const char *ext = strrchr (fname, '.');
+    if (ext && (!strcasecmp (ext, ".rar") || !strcasecmp (ext, ".gz") || !strcasecmp (ext, ".7z") )) {
+        return 1;
+    }
+    return 0;
+}
+
+/* boilerplate */
+
+static DB_vfs_t plugin = {
+    .plugin.api_vmajor = 1,
+    .plugin.api_vminor = 0,
+    .plugin.version_major = 1,
+    .plugin.version_minor = 0,
+    .plugin.type = DB_PLUGIN_VFS,
+    .plugin.id = "vfs_archive_reader",
+    .plugin.name = "RAR, 7z and Gzip archive reader",
+    .plugin.descr = "play files directly from rar,7z and gzip archive files",
+    .plugin.copyright = 
+        "Carlos.\n"
+    ,
+    .plugin.website = "none",
+    .open = vfs_archive_reader_open,
+    .close = vfs_archive_reader_close,
+    .read = vfs_archive_reader_read,
+    .seek = vfs_archive_reader_seek,
+    .tell = vfs_archive_reader_tell,
+    .rewind = vfs_archive_reader_rewind,
+    .getlength = vfs_archive_reader_getlength,
+    .get_schemes = vfs_archive_reader_get_schemes,
+    .is_streaming = vfs_archive_reader_is_streaming,
+    .is_container = vfs_archive_reader_is_container,
+    .scandir = vfs_archive_reader_scandir,
+};
+
+DB_plugin_t *
+vfs_archive_reader_load (DB_functions_t *api) {
+
+    deadbeef = api;
+    return DB_PLUGIN (&plugin);
+}
