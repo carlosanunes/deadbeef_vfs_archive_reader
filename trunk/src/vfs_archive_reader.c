74ac7e16dc56d021e4fb088f8499d8d24038c925
carlos.nunes <carlos.nunes@e452b2a4-3fef-4c0f-9a7f-398e19c9e114>
2011-08-01 20:42:25 +0000

    reverted to memory heavy version

--- trunk/src/vfs_archive_reader.c
+++ trunk/src/vfs_archive_reader.c
@@ -17,8 +17,11 @@ static DB_vfs_t plugin;
 
 typedef struct {
     DB_FILE file;
-    FILE* stream;
+    fex_t* archive;
+    int64_t size;
+    int64_t tell; // current position in file
 } archive_file_t;
+
 
 static const char *scheme_names[] = { "rar://", "7z://", "gz://", NULL };
 
@@ -36,13 +39,13 @@ vfs_archive_reader_is_streaming (void) {
 DB_FILE*
 vfs_archive_reader_open (const char *fname) {
 
-	if ( (strncasecmp(fname, "rar://", 6) != 0 ) && (strncasecmp(fname, "7z://", 5) != 0 ) && (strncasecmp(fname, "gz://", 5) != 0  ))
-		{ return NULL; }    
+        if ( (strncasecmp(fname, "rar://", 6) != 0 ) && (strncasecmp(fname, "7z://", 5) != 0 ) && (strncasecmp(fname, "gz://", 5) != 0  ))
+                { return NULL; }    
 
     if (strncasecmp (fname, "rar://", 6) == 0)
-	fname += 6;
+        fname += 6;
     else
-	fname += 5;
+        fname += 5;
 
     const char *colon = strchr (fname, ':');
     if (!colon) {
@@ -55,44 +58,33 @@ vfs_archive_reader_open (const char *fname) {
 
     fname = colon+1;
 
- 	fex_t* fex;
-	if (fex_open( &fex, archname) != NULL ) /* opening the rar */
-		{ fex_close(fex); return NULL; }
+        fex_t* fex;
+        if (fex_open( &fex, archname) != NULL ) /* opening the rar */
+                { fex_close(fex); return NULL; }
 
-	/* the library doesn have a built in search function so */
-	int n = 0; 
-	while ( !fex_done( fex ) )
-	{
-		if(strcmp(fex_name(fex), fname) == 0 )
-			break; /* found the one */
+        /* the library doesn have a built in search function so */
+        int n = 0; 
+        while ( !fex_done( fex ) )
+        {
+                if(strcmp(fex_name(fex), fname) == 0 )
+                        break; /* found the one */
 
-		++n;
-		fex_next( fex );
-	}
-	//  are we at the end of the archive?
-	if(fex_done(fex))
-		{ fex_close(fex); return NULL; }
+                ++n;
+                fex_next( fex );
+        }
+        //  are we at the end of the archive?
+        if(fex_done(fex))
+                { fex_close(fex); return NULL; }
 
-	char template[] = "/tmp/db.vfs.XXXXXX";
-	int tmp = mkstemp(template);
-	if ( tmp < 0 ) // could not create the tmpfile
-		return NULL;	
-	FILE * tmpFile = fdopen(tmp, "w+");
-	assert(tmpFile != NULL);	
-
-	// extracting to tmpfile
-	fex_stat(fex);
-	const void* data;
-	if ( fex_data( fex, &data ) != NULL )
-		assert( 0 );	
-	fwrite(data, sizeof(unsigned char), fex_size(fex), tmpFile);	
-
-	fex_close(fex);
+        if (fex_stat(fex) != NULL)
+                { fex_close(fex); return NULL; }
 
     archive_file_t *f = malloc (sizeof (archive_file_t));
     memset (f, 0, sizeof (archive_file_t));
-    f->stream = tmpFile;
-    f->file.vfs = &plugin;	
+    f->size = (int64_t) fex_size(fex);
+    f->archive = fex;
+    f->tell = 0;
+    f->file.vfs = &plugin;      
 
     return (DB_FILE*)f;
 
@@ -100,79 +92,99 @@ vfs_archive_reader_open (const char *fname) {
 
 void
 vfs_archive_reader_close (DB_FILE *f) {
-	archive_file_t *arc = (archive_file_t *)f;
-	fclose(arc->stream);
-	free(arc);
+        archive_file_t *arc = (archive_file_t *)f;
+        fex_close(arc->archive);
+        free(arc);
 }
 
 size_t
 vfs_archive_reader_read (void *ptr, size_t size, size_t nmemb, DB_FILE *f) {
-	archive_file_t *arc = (archive_file_t *)f;
+        archive_file_t *arc = (archive_file_t *)f;
 
-	return fread (ptr, size, nmemb, arc->stream);
+        int n = min ( size * nmemb, arc->size - arc->tell );
+        
+        // Fine to call fex_data() every time, since it internally caches data in memory
+        // for ALL file types when you use fex_data().
+        const void* data;
+        if ( fex_data( arc->archive, &data ) != NULL )
+                assert( 0 );
+        
+        memcpy( ptr, (const char*) data + arc->tell, n );
+        arc->tell += n;
+
+        // Do NOT free data since fex manages it internally
+        
+
+        return n/size;
 }
 
 int
 vfs_archive_reader_seek (DB_FILE *f, int64_t offset, int whence) {
-	archive_file_t *arc = (archive_file_t *)f;
+        archive_file_t *arc = (archive_file_t *)f;
 
-    return fseek (arc->stream, offset, whence);
+    if (whence == SEEK_CUR) {
+        offset += arc->tell;
+    } else if (whence == SEEK_END) {
+        offset += (int64_t) arc->size;
+    }
+        
+        if ( offset < 0 || offset > arc->size )
+                return -1;
+        
+        arc->tell = offset;
+    return 0;
 }
 
 int64_t
 vfs_archive_reader_tell (DB_FILE *f) {
     archive_file_t *arc = (archive_file_t *)f;
-    return ftell (arc->stream);
+    return (int64_t) arc->tell;
 }
 
 
 void
 vfs_archive_reader_rewind (DB_FILE *f) {
-	archive_file_t *arc = (archive_file_t *)f;
-	rewind (arc->stream);
+        archive_file_t *arc = (archive_file_t *)f;
+        arc->tell = 0;
 }
 
 
 int64_t
 vfs_archive_reader_getlength (DB_FILE *f) {
-	archive_file_t *arc = (archive_file_t *)f;
-    size_t offs = ftell (arc->stream);
-    fseek (arc->stream, 0, SEEK_END);
-    size_t l = ftell (arc->stream);
-    fseek (arc->stream, offs, SEEK_SET);
-    return l;
+        archive_file_t *arc = (archive_file_t *)f;
+    return arc->size;
 }
 
 /* getting the contents  */
 int
 vfs_archive_reader_scandir (const char *dir, struct dirent ***namelist, int (*selector) (const struct dirent *), int (*cmp) (const struct dirent **, const struct dirent **)) {
-	
-	fex_t* fex;
-	if (fex_open( &fex, dir) != NULL )
-		return -1;
-	
-	int n = 0;
-	char * ext = NULL;
-	while ( !fex_done(fex) )
-	{
-		*namelist = realloc (*namelist, sizeof (void *) * (n+1) );
-		(*namelist)[n] = malloc (sizeof (struct dirent));
-		memset ((*namelist)[n], 0, sizeof (struct dirent));
-		if (fex_has_extension( dir, "rar" ))
-			ext = "rar";
-		else if (fex_has_extension(dir, "gz"))
-			ext = "gz";
-		else
-			ext = "7z";
-		snprintf ((*namelist)[n]->d_name, sizeof ((*namelist)[n]->d_name), "%s://%s:%s", ext, dir, fex_name(fex) );
+        
+        fex_t* fex;
+        if (fex_open( &fex, dir) != NULL )
+                return -1;
+        
+        int n = 0;
+        char * ext = NULL;
+        while ( !fex_done(fex) )
+        {
+                *namelist = realloc (*namelist, sizeof (void *) * (n+1) );
+                (*namelist)[n] = malloc (sizeof (struct dirent));
+                memset ((*namelist)[n], 0, sizeof (struct dirent));
+                if (fex_has_extension( dir, "rar" ))
+                        ext = "rar";
+                else if (fex_has_extension(dir, "gz"))
+                        ext = "gz";
+                else
+                        ext = "7z";
+                snprintf ((*namelist)[n]->d_name, sizeof ((*namelist)[n]->d_name), "%s://%s:%s", ext, dir, fex_name(fex) );
 
-		fex_next(fex);
-		++n;
-	}
+                fex_next(fex);
+                ++n;
+        }
 
-	fex_close(fex);
+        fex_close(fex);
 
-	return n;
+        return n;
 }
 
 
