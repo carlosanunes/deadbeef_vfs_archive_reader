71c2a03e3faa90d2bb70fbec3370bcf145bb39de
carlos.nunes <carlosanunes@users.noreply.github.com>
2012-04-11 18:56:50 +0000

    added copying file and license to vfs_archive_reader.c header

--- src/vfs_archive_reader.c
+++ src/vfs_archive_reader.c
@@ -1,3 +1,23 @@
+/*
+	RAR, 7z and Gzip archive reader
+	Copyright (c) 2011-2012 Carlos Nunes <carloslnunes@gmail.com>.
+	Uses the File_Extractor 1.0.0 library (C) Shay Green, http://code.google.com/p/file-extractor/
+
+	RAR, 7z and Gzip archive reader is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	RAR, 7z and Gzip archive reader is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include <deadbeef/deadbeef.h>
 #include <strings.h>
 #include <string.h>
