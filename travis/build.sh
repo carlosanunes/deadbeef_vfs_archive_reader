167d94eea26f1592f1534f7b119499421f488cb8
carlosanunes <carloslnunes@gmail.com>
2016-02-14 23:27:58 +0000

    simplified build script

--- travis/build.sh
+++ travis/build.sh
@@ -4,14 +4,11 @@ case "$TRAVIS_OS_NAME" in
         cd src
         echo "building for i686"
         ARCH=i686 make || exit 1
-        cd ../build/linux/
-        tar -jcfv ddb_archive_reader_i686.tar.bz2 *.so || exit 1
-        cd ../../src
+        tar -cvjSf ../build/linux/ddb_archive_reader_i686.tar.bz2  ../build/linux/*.so || exit 1
         echo "building for x86_64"
         ARCH=x86_64 make || exit 1
-        cd ../build/linux/
-        tar -jcfv ddb_archive_reader_x86_64.tar.bz2 *.so || exit 1
-        cd ../..
+        tar -cvjSf ../build/linux/ddb_archive_reader_x86_64.tar.bz2  ../build/linux/*.so || exit 1
+        cd ..
     ;;
     osx)
         cd src
