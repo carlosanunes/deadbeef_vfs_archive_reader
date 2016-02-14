bce523a15da6557d6f70d8cc9de417cfabf047bf
carlosanunes <carloslnunes@gmail.com>
2016-02-14 18:45:15 +0000

    fixed upload script

--- travis/upload.sh
+++ travis/upload.sh
@@ -5,11 +5,11 @@ echo uploading $TRAVIS_OS_NAME build
 case "$TRAVIS_OS_NAME" in
 	linux)
 		cd build/linux
-		curl -u "${BOX_USERNAME}@${BOX_PASSWORD}" -T ddb_archive_reader_i686.tar.bz2 https://dav.box.com/dav/ddb_archive_reader_i686.tar.bz2 > /dev/null 2>&1
-		curl -u "${BOX_USERNAME}@${BOX_PASSWORD}" -T ddb_archive_reader_x86_64.tar.bz2 https://dav.box.com/dav/ddb_archive_reader_x86_64.tar.bz2 > /dev/null 2>&1
+		curl -u "${BOX_USERNAME}:${BOX_PASSWORD}" -T ddb_archive_reader_i686.tar.bz2 https://dav.box.com/dav/ddb_archive_reader_i686.tar.bz2 > /dev/null 2>&1
+		curl -u "${BOX_USERNAME}:${BOX_PASSWORD}" -T ddb_archive_reader_x86_64.tar.bz2 https://dav.box.com/dav/ddb_archive_reader_x86_64.tar.bz2 > /dev/null 2>&1
 	;;
 	osx)
 		cd build/osx
-		curl -u "${BOX_USERNAME}@${BOX_PASSWORD}" -T ddb_archive_reader_osx.zip https://dav.box.com/dav/ddb_archive_reader_osx.zip > /dev/null 2>&1
+		curl -u "${BOX_USERNAME}:${BOX_PASSWORD}" -T ddb_archive_reader_osx.zip https://dav.box.com/dav/ddb_archive_reader_osx.zip > /dev/null 2>&1
 	;;
 esac
