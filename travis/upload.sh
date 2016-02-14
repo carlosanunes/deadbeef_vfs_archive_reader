cdb7f412233255fb5a0f4bc456cc8c929cbd274c
carlosanunes <carloslnunes@gmail.com>
2016-02-14 18:43:32 +0000

    changed travis configuration to send build artifacts to box storage.

--- travis/upload.sh
+++ travis/upload.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+echo uploading $TRAVIS_OS_NAME build
 case "$TRAVIS_OS_NAME" in
 	linux)
 		cd build/linux
+		curl -u "${BOX_USERNAME}@${BOX_PASSWORD}" -T ddb_archive_reader_i686.tar.bz2 https://dav.box.com/dav/ddb_archive_reader_i686.tar.bz2 > /dev/null 2>&1
+		curl -u "${BOX_USERNAME}@${BOX_PASSWORD}" -T ddb_archive_reader_x86_64.tar.bz2 https://dav.box.com/dav/ddb_archive_reader_x86_64.tar.bz2 > /dev/null 2>&1
 	;;
 	osx)
 		cd build/osx
+		curl -u "${BOX_USERNAME}@${BOX_PASSWORD}" -T ddb_archive_reader_osx.zip https://dav.box.com/dav/ddb_archive_reader_osx.zip > /dev/null 2>&1
 	;;
 esac
-
-# dummy repo
-git init
-
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "dummy@email.com"
-
-git add .
-git commit -m "Build push"
-
-echo uploading $TRAVIS_OS_NAME build
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:releases
