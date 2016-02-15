7b1b5aa05ddae04eb127fd5aca05a12dc500b7fd
carlosanunes <carloslnunes@gmail.com>
2016-02-15 18:55:29 +0000

    reverted build config to create the plugin in the same dir as the makefile

--- travis/build.sh
+++ travis/build.sh
@@ -4,16 +4,16 @@ case "$TRAVIS_OS_NAME" in
         cd src
         echo "building for i686"
         ARCH=i686 make || exit 1
-        tar -cvjSf ../build/linux/ddb_archive_reader_i686.tar.bz2  ../build/linux/*.so || exit 1
+        tar -cvjSf ../ddb_archive_reader_i686.tar.bz2  ddb_archive_reader.so || exit 1
         echo "building for x86_64"
         ARCH=x86_64 make || exit 1
-        tar -cvjSf ../build/linux/ddb_archive_reader_x86_64.tar.bz2  ../build/linux/*.so || exit 1
+        tar -cvjSf ../ddb_archive_reader_x86_64.tar.bz2  ddb_archive_reader.so || exit 1
         cd ..
     ;;
     osx)
         cd src
         make || exit 1
-        zip -r ../build/osx/ddb_archive_reader_osx.zip ../build/osx/* || exit 1
+        zip -r ../ddb_archive_reader_osx.zip ddb_archive_reader.dylib || exit 1
         cd ..
     ;;
 esac
